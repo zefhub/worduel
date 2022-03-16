a08d8087e4d95a169185510561baa9c96bd154e1
Daniel Dyrnes <thedanielforum@gmail.com>
2022-03-16 12:12:18 +0900

    Update run_api.py

--- backend/run_api.py
+++ backend/run_api.py
@@ -10,10 +10,11 @@ if __name__ == "__main__":
     g = Graph(worduel_tag)
     # make_primary(g, True)
     Effect({
-        "type": FX.GraphQL.StartPlayground,
+        "type": FX.GraphQL.StartServer,
         "schema_root": gql_schema(g),
         "port": 5010,
         "open_browser": False,
+        "bind_address": "0.0.0.0",
     }) | run
     while True:
         sleep(1)
